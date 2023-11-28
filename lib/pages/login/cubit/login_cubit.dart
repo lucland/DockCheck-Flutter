8171d9882968e80ba7c00286221171d0a6843a65
lucland <lucas.vsc@gmail.com>
2023-11-28 16:59:51 -0300

    state refactored

--- lib/pages/login/cubit/login_cubit.dart
+++ lib/pages/login/cubit/login_cubit.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-enum LoginState { initial, loading, success, error }
+import 'login_state.dart';
 
 class LoginCubit extends Cubit<LoginState> {
   LoginCubit() : super(LoginState.initial);
@@ -8,7 +8,6 @@ class LoginCubit extends Cubit<LoginState> {
   Future<void> logIn(String email, String password) async {
     emit(LoginState.loading);
     try {
-      // Implement your login logic here
       emit(LoginState.success);
     } catch (e) {
       emit(LoginState.error);
