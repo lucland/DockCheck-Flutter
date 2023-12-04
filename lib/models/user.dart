cf5957b0ca4496fb92e75313e889d7e414486ab7
lucland <lucas.vsc@gmail.com>
2023-12-04 16:50:00 -0300

    Fix bug in login functionality

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,177 +1,279 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-
 class User {
-  int numero;
+  String id;
+  List<String> authorizationsId;
+  String name;
+  String company;
+  String role;
+  String project;
+  int number;
   String identidade;
-  String nome;
-  String funcao;
+  String cpf;
+  DateTime aso;
+  String asoDocument;
+  bool hasAso;
+  DateTime nr34;
+  String nr34Document;
+  bool hasNr34;
+  DateTime nr35;
+  String nr35Document;
+  bool hasNr35;
+  DateTime nr33;
+  String nr33Document;
+  bool hasNr33;
+  DateTime nr10;
+  String nr10Document;
+  bool hasNr10;
   String email;
-  String empresa;
-  Timestamp ASO;
-  Timestamp NR34;
-  Timestamp NR10;
-  Timestamp NR33;
-  Timestamp NR35;
-  String vessel;
-  Timestamp dataInicial;
-  Timestamp dataLimite;
-  bool isVisitante;
-  bool isAdmin;
-  List<String> eventos;
-  Timestamp createdAt;
-  Timestamp updatedAt;
-  bool isBlocked;
   String area;
-  String reason;
+  bool isAdmin;
+  bool isVisitor;
+  bool isGuardian;
   bool isOnboarded;
-  bool isSupervisor;
-  String password;
+  bool isBlocked;
+  String blockReason;
+  String rfid;
+  String picture;
+  DateTime createdAt;
+  DateTime updatedAt;
+  List<String> events;
+  String typeJob;
+  DateTime startJob;
+  DateTime endJob;
+  String username;
+  String salt;
+  String hash;
 
   User({
-    required this.numero,
+    required this.id,
+    required this.authorizationsId,
+    required this.name,
+    required this.company,
+    required this.role,
+    required this.project,
+    required this.number,
     required this.identidade,
-    required this.nome,
-    required this.funcao,
+    required this.cpf,
+    required this.aso,
+    required this.asoDocument,
+    required this.hasAso,
+    required this.nr34,
+    required this.nr34Document,
+    required this.hasNr34,
+    required this.nr35,
+    required this.nr35Document,
+    required this.hasNr35,
+    required this.nr33,
+    required this.nr33Document,
+    required this.hasNr33,
+    required this.nr10,
+    required this.nr10Document,
+    required this.hasNr10,
     required this.email,
-    required this.empresa,
-    required this.ASO,
-    required this.NR34,
-    required this.NR10,
-    required this.NR33,
-    required this.NR35,
-    required this.vessel,
-    required this.dataInicial,
-    required this.dataLimite,
-    required this.isVisitante,
+    required this.area,
     required this.isAdmin,
-    required this.eventos,
+    required this.isVisitor,
+    required this.isGuardian,
+    required this.isOnboarded,
+    required this.isBlocked,
+    required this.blockReason,
+    required this.rfid,
+    required this.picture,
     required this.createdAt,
     required this.updatedAt,
-    required this.isBlocked,
-    required this.area,
-    required this.reason,
-    required this.isOnboarded,
-    required this.isSupervisor,
-    required this.password,
+    required this.events,
+    required this.typeJob,
+    required this.startJob,
+    required this.endJob,
+    required this.username,
+    required this.salt,
+    required this.hash,
   });
 
-  User copyWith({
-    int? numero,
-    String? identidade,
-    String? nome,
-    String? funcao,
-    String? email,
-    String? empresa,
-    Timestamp? ASO,
-    Timestamp? NR34,
-    Timestamp? NR10,
-    Timestamp? NR33,
-    Timestamp? NR35,
-    String? vessel,
-    Timestamp? dataInicial,
-    Timestamp? dataLimite,
-    bool? isVisitante,
-    bool? isAdmin,
-    List<String>? eventos,
-    Timestamp? createdAt,
-    Timestamp? updatedAt,
-    bool? isBlocked,
-    String? area,
-    String? reason,
-    bool? isOnboarded,
-    bool? isSupervisor,
-    String? password,
-  }) {
+  factory User.fromJson(Map<String, dynamic> json) {
     return User(
-      numero: numero ?? this.numero,
-      identidade: identidade ?? this.identidade,
-      nome: nome ?? this.nome,
-      funcao: funcao ?? this.funcao,
-      email: email ?? this.email,
-      empresa: empresa ?? this.empresa,
-      ASO: ASO ?? this.ASO,
-      NR34: NR34 ?? this.NR34,
-      NR10: NR10 ?? this.NR10,
-      NR33: NR33 ?? this.NR33,
-      NR35: NR35 ?? this.NR35,
-      vessel: vessel ?? this.vessel,
-      dataInicial: dataInicial ?? this.dataInicial,
-      dataLimite: dataLimite ?? this.dataLimite,
-      isVisitante: isVisitante ?? this.isVisitante,
-      isAdmin: isAdmin ?? this.isAdmin,
-      eventos: eventos ?? this.eventos,
-      createdAt: createdAt ?? this.createdAt,
-      updatedAt: updatedAt ?? this.updatedAt,
-      isBlocked: isBlocked ?? this.isBlocked,
-      area: area ?? this.area,
-      reason: reason ?? this.reason,
-      isOnboarded: isOnboarded ?? this.isOnboarded,
-      isSupervisor: isSupervisor ?? this.isSupervisor,
-      password: password ?? this.password,
+      id: json['id'],
+      authorizationsId: List<String>.from(json['authorizations_id']),
+      name: json['name'],
+      company: json['company'],
+      role: json['role'],
+      project: json['project'],
+      number: json['number'],
+      identidade: json['identidade'],
+      cpf: json['cpf'],
+      aso: DateTime.parse(json['aso']),
+      asoDocument: json['aso_document'],
+      hasAso: json['has_aso'],
+      nr34: DateTime.parse(json['nr34']),
+      nr34Document: json['nr34_document'],
+      hasNr34: json['has_nr34'],
+      nr35: DateTime.parse(json['nr35']),
+      nr35Document: json['nr35_document'],
+      hasNr35: json['has_nr35'],
+      nr33: DateTime.parse(json['nr33']),
+      nr33Document: json['nr33_document'],
+      hasNr33: json['has_nr33'],
+      nr10: DateTime.parse(json['nr10']),
+      nr10Document: json['nr10_document'],
+      hasNr10: json['has_nr10'],
+      email: json['email'],
+      area: json['area'],
+      isAdmin: json['is_admin'],
+      isVisitor: json['is_visitor'],
+      isGuardian: json['is_guardian'],
+      isOnboarded: json['is_onboarded'],
+      isBlocked: json['is_blocked'],
+      blockReason: json['block_reason'],
+      rfid: json['rfid'],
+      picture: json['picture'],
+      createdAt: DateTime.parse(json['created_at']),
+      updatedAt: DateTime.parse(json['updated_at']),
+      events: List<String>.from(json['events']),
+      typeJob: json['type_job'],
+      startJob: DateTime.parse(json['start_job']),
+      endJob: DateTime.parse(json['end_job']),
+      username: json['username'],
+      salt: json['salt'],
+      hash: json['hash'],
     );
   }
 
-  Map<String, dynamic> toMap() {
+  Map<String, dynamic> toJson() {
     return {
-      'numero': numero,
+      'id': id,
+      'authorizations_id': authorizationsId,
+      'name': name,
+      'company': company,
+      'role': role,
+      'project': project,
+      'number': number,
       'identidade': identidade,
-      'nome': nome,
-      'funcao': funcao,
+      'cpf': cpf,
+      'aso': aso.toIso8601String(),
+      'aso_document': asoDocument,
+      'has_aso': hasAso,
+      'nr34': nr34.toIso8601String(),
+      'nr34_document': nr34Document,
+      'has_nr34': hasNr34,
+      'nr35': nr35.toIso8601String(),
+      'nr35_document': nr35Document,
+      'has_nr35': hasNr35,
+      'nr33': nr33.toIso8601String(),
+      'nr33_document': nr33Document,
+      'has_nr33': hasNr33,
+      'nr10': nr10.toIso8601String(),
+      'nr10_document': nr10Document,
+      'has_nr10': hasNr10,
       'email': email,
-      'empresa': empresa,
-      'ASO': ASO,
-      'NR34': NR34,
-      'NR10': NR10,
-      'NR33': NR33,
-      'NR35': NR35,
-      'vessel': vessel,
-      'dataInicial': dataInicial,
-      'dataLimite': dataLimite,
-      'isVisitante': isVisitante,
-      'isAdmin': isAdmin,
-      'eventos': eventos,
-      'createdAt': createdAt,
-      'updatedAt': updatedAt,
-      'isBlocked': isBlocked,
       'area': area,
-      'reason': reason,
-      'isOnboarded': isOnboarded,
-      'isSupervisor': isSupervisor,
-      'password': password,
+      'is_admin': isAdmin,
+      'is_visitor': isVisitor,
+      'is_guardian': isGuardian,
+      'is_onboarded': isOnboarded,
+      'is_blocked': isBlocked,
+      'block_reason': blockReason,
+      'rfid': rfid,
+      'picture': picture,
+      'created_at': createdAt.toIso8601String(),
+      'updated_at': updatedAt.toIso8601String(),
+      'events': events,
+      'type_job': typeJob,
+      'start_job': startJob.toIso8601String(),
+      'end_job': endJob.toIso8601String(),
+      'username': username,
+      'salt': salt,
+      'hash': hash,
     };
   }
 
-  static User fromMap(Map<String, dynamic> map) {
+  User copyWith({
+    String? id,
+    List<String>? authorizationsId,
+    String? name,
+    String? company,
+    String? role,
+    String? project,
+    int? number,
+    String? identidade,
+    String? cpf,
+    DateTime? aso,
+    String? asoDocument,
+    bool? hasAso,
+    DateTime? nr34,
+    String? nr34Document,
+    bool? hasNr34,
+    DateTime? nr35,
+    String? nr35Document,
+    bool? hasNr35,
+    DateTime? nr33,
+    String? nr33Document,
+    bool? hasNr33,
+    DateTime? nr10,
+    String? nr10Document,
+    bool? hasNr10,
+    String? email,
+    String? area,
+    bool? isAdmin,
+    bool? isVisitor,
+    bool? isGuardian,
+    bool? isOnboarded,
+    bool? isBlocked,
+    String? blockReason,
+    String? rfid,
+    String? picture,
+    DateTime? createdAt,
+    DateTime? updatedAt,
+    List<String>? events,
+    String? typeJob,
+    DateTime? startJob,
+    DateTime? endJob,
+    String? username,
+    String? salt,
+    String? hash,
+  }) {
     return User(
-      numero: map['numero'],
-      identidade: map['identidade'],
-      nome: map['nome'],
-      funcao: map['funcao'],
-      email: map['email'],
-      empresa: map['empresa'],
-      ASO: map['ASO'],
-      NR34: map['NR34'],
-      NR10: map['NR10'],
-      NR33: map['NR33'],
-      NR35: map['NR35'],
-      vessel: map['vessel'],
-      dataInicial: map['dataInicial'],
-      dataLimite: map['dataLimite'],
-      isVisitante: map['isVisitante'],
-      isAdmin: map['isAdmin'],
-      eventos: List<String>.from(map['eventos']),
-      createdAt: map['createdAt'],
-      updatedAt: map['updatedAt'],
-      isBlocked: map['isBlocked'],
-      area: map['area'],
-      reason: map['reason'],
-      isOnboarded: map['isOnboarded'],
-      isSupervisor: map['isSupervisor'],
-      password: map['password'],
+      id: id ?? this.id,
+      authorizationsId: authorizationsId ?? this.authorizationsId,
+      name: name ?? this.name,
+      company: company ?? this.company,
+      role: role ?? this.role,
+      project: project ?? this.project,
+      number: number ?? this.number,
+      identidade: identidade ?? this.identidade,
+      cpf: cpf ?? this.cpf,
+      aso: aso ?? this.aso,
+      asoDocument: asoDocument ?? this.asoDocument,
+      hasAso: hasAso ?? this.hasAso,
+      nr34: nr34 ?? this.nr34,
+      nr34Document: nr34Document ?? this.nr34Document,
+      hasNr34: hasNr34 ?? this.hasNr34,
+      nr35: nr35 ?? this.nr35,
+      nr35Document: nr35Document ?? this.nr35Document,
+      hasNr35: hasNr35 ?? this.hasNr35,
+      nr33: nr33 ?? this.nr33,
+      nr33Document: nr33Document ?? this.nr33Document,
+      hasNr33: hasNr33 ?? this.hasNr33,
+      nr10: nr10 ?? this.nr10,
+      nr10Document: nr10Document ?? this.nr10Document,
+      hasNr10: hasNr10 ?? this.hasNr10,
+      email: email ?? this.email,
+      area: area ?? this.area,
+      isAdmin: isAdmin ?? this.isAdmin,
+      isVisitor: isVisitor ?? this.isVisitor,
+      isGuardian: isGuardian ?? this.isGuardian,
+      isOnboarded: isOnboarded ?? this.isOnboarded,
+      isBlocked: isBlocked ?? this.isBlocked,
+      blockReason: blockReason ?? this.blockReason,
+      rfid: rfid ?? this.rfid,
+      picture: picture ?? this.picture,
+      createdAt: createdAt ?? this.createdAt,
+      updatedAt: updatedAt ?? this.updatedAt,
+      events: events ?? this.events,
+      typeJob: typeJob ?? this.typeJob,
+      startJob: startJob ?? this.startJob,
+      endJob: endJob ?? this.endJob,
+      username: username ?? this.username,
+      salt: salt ?? this.salt,
+      hash: hash ?? this.hash,
     );
-  }
-
-  String toDatabaseString() {
-    return "$numero\r\n$nome\r\n$empresa\r\n$funcao\r\n$identidade\r\n$email\r\n$vessel\r\n$ASO\r\n$NR34\r\n$NR10\r\n$NR33\r\n$NR35\r\n$dataInicial\r\n$dataLimite";
   }
 }
