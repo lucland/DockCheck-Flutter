0e14d6b1be6f936eb25e11e9a35e0485ad46e651
lucland <lucas.vsc@gmail.com>
2023-12-04 16:52:33 -0300

    Refactor UserRepository class to use ApiService and LocalStorageService

--- lib/repositories/user_repository.dart
+++ lib/repositories/user_repository.dart
@@ -1,175 +1,92 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:cripto_qr_googlemarine/models/user.dart';
-import 'package:cripto_qr_googlemarine/utils/simple_logger.dart';
+import 'package:cripto_qr_googlemarine/services/api_service.dart';
+import 'package:cripto_qr_googlemarine/services/local_storage_service.dart';
+
+import '../models/authorization.dart';
 
 class UserRepository {
-  final CollectionReference _usersCollection =
-      FirebaseFirestore.instance.collection('USER');
-  final CollectionReference _eventsCollection =
-      FirebaseFirestore.instance.collection('EVENTO');
+  final ApiService apiService;
+  final LocalStorageService localStorageService;
 
-  Future<void> addUser(Map<String, dynamic> userMap, String identidade) async {
+  UserRepository(this.apiService, this.localStorageService);
+
+  Future<User> createUser(User user) async {
+    final data = await apiService.post('users/create', user.toJson());
+    return User.fromJson(data);
+  }
+
+  Future<User> getUser(String id) async {
+    final data = await apiService.get('users/$id');
+    return User.fromJson(data);
+  }
+
+  Future<User> updateUser(String id, User user) async {
+    final data = await apiService.put('users/$id', user.toJson());
+    return User.fromJson(data);
+  }
+
+  Future<void> deleteUser(String id) async {
+    await apiService.delete('users/$id');
+  }
+
+  Future<List<User>> getAllUsers({int limit = 10, int offset = 0}) async {
+    final data = await apiService.get('users?limit=$limit&offset=$offset');
+    return (data as List).map((item) => User.fromJson(item)).toList();
+  }
+
+  // Get User Authorizations
+  Future<List<Authorization>> getUserAuthorizations(String userId) async {
+    final data = await apiService.get('users/$userId/authorizations');
+    return (data as List).map((item) => Authorization.fromJson(item)).toList();
+  }
+
+  // Check Username Availability
+  Future<bool> checkUsername(String username) async {
+    final data =
+        await apiService.post('users/checkUsername', {'username': username});
+    return data['message'] == 'Username available';
+  }
+
+  // Search Users
+  Future<List<User>> searchUsers(
+      String searchTerm, int page, int pageSize) async {
+    final data = await apiService.get(
+        'users/search?searchTerm=$searchTerm&page=$page&pageSize=$pageSize');
+    return (data['users'] as List).map((item) => User.fromJson(item)).toList();
+  }
+
+  // Get Last User Number
+  Future<int> getLastUserNumber() async {
+    final data = await apiService.get('users/all/lastnumber');
+    return int.parse(data.toString());
+  }
+
+  // Get Valid Users by Vessel ID
+  Future<List<String>> getValidUsersByVesselID(String vesselId) async {
+    final data = await apiService.get('users/valids/$vesselId');
+    return List<String>.from(data);
+  }
+
+  // Sync users from the server and update local database
+  Future<void> syncUsers() async {
     try {
-      await _usersCollection.doc(identidade).set(userMap);
-      SimpleLogger.fine("User Added: ${userMap['name']}");
-    } on FirebaseException catch (error) {
-      SimpleLogger.warning("Failed to add user: $error");
-      throw Exception("Failed to add user");
-    } on Exception catch (error) {
-      SimpleLogger.warning("Failed to add user: $error");
-      throw Exception("Failed to add user");
+      // Fetch data from the server
+      final serverUsers = await getAllUsers();
+
+      // Update local database
+      await updateLocalDatabase(serverUsers);
+    } catch (e) {
+      // Handle errors
     }
   }
 
-  Future<void> addEvent(Map<String, dynamic> eventMap) async {
-    try {
-      await _eventsCollection.add(eventMap);
-      SimpleLogger.fine("Event Added: ${eventMap['acao']}");
-    } on FirebaseException catch (error) {
-      SimpleLogger.warning("Failed to add event: $error");
-      throw Exception("Failed to add event");
-    } on Exception catch (error) {
-      SimpleLogger.warning("Failed to add event: $error");
-      throw Exception("Failed to add event");
-    }
-  }
+  Future<void> updateLocalDatabase(List<User> serverUsers) async {
+    // Clear local data
+    await localStorageService.clearTable('users');
 
-  Future<List<Map<String, dynamic>>> fetchEvents(String identidade) async {
-    try {
-      SimpleLogger.info("Fetching events");
-      QuerySnapshot querySnapshot =
-          await _eventsCollection.where('user', isEqualTo: identidade).get();
-      return querySnapshot.docs
-          .map((doc) => doc.data() as Map<String, dynamic>)
-          .toList();
-    } on FirebaseException catch (error) {
-      SimpleLogger.warning("Failed to fetch events: $error");
-      throw Exception("Failed to fetch events");
-    } catch (error) {
-      SimpleLogger.warning("Failed to fetch events: $error");
-      throw Exception("Failed to fetch events");
-    }
-  }
-
-  Future<User> fetchLastUser() async {
-    try {
-      SimpleLogger.info("Fetching last user");
-      QuerySnapshot querySnapshot = await _usersCollection
-          .orderBy('createdAt', descending: true)
-          .limit(1)
-          .get();
-      var data = querySnapshot.docs[0].data() as Map<String, dynamic>;
-      return User.fromMap(data);
-    } on FirebaseException catch (error) {
-      SimpleLogger.warning("Failed to fetch last user: $error");
-      throw Exception("Failed to fetch last user");
-    } catch (error) {
-      SimpleLogger.warning("Failed to fetch last user: $error");
-      throw Exception("Failed to fetch last user");
-    }
-  }
-
-  Future<List<Map<String, dynamic>>> fetchUsers() async {
-    try {
-      SimpleLogger.info("Fetching users");
-      QuerySnapshot querySnapshot = await _usersCollection.get();
-      return querySnapshot.docs
-          .map((doc) => doc.data() as Map<String, dynamic>)
-          .toList();
-    } on FirebaseException catch (error) {
-      SimpleLogger.warning("Failed to fetch users: $error");
-      throw Exception("Failed to fetch users");
-    } catch (error) {
-      SimpleLogger.warning("Failed to fetch users: $error");
-      throw Exception("Failed to fetch users");
-    }
-  }
-
-  Future<List<Map<String, dynamic>>> searchUsers(String query) async {
-    try {
-      SimpleLogger.info("Searching users");
-
-      List<Map<String, dynamic>> results = [];
-
-      if (RegExp(r'^\d+$').hasMatch(query)) {
-        QuerySnapshot querySnapshotIdentidade =
-            await _usersCollection.where('identidade', isEqualTo: query).get();
-        results = querySnapshotIdentidade.docs
-            .map((doc) => doc.data() as Map<String, dynamic>)
-            .toList();
-      } else {
-        QuerySnapshot querySnapshotNome = await _usersCollection
-            .where('nome', isGreaterThanOrEqualTo: query)
-            .get();
-        results = querySnapshotNome.docs
-            .map((doc) => doc.data() as Map<String, dynamic>)
-            .toList();
-      }
-
-      print(results);
-
-      return results;
-    } catch (error) {
-      SimpleLogger.warning("Failed to search users: $error");
-      throw Exception("Failed to search users");
-    }
-  }
-
-  Future<void> updateUserData(
-      String identidade, String field, dynamic data) async {
-    try {
-      SimpleLogger.info("Updating user data");
-      await _usersCollection.doc(identidade).update({field: data});
-    } catch (error) {
-      SimpleLogger.warning("Failed to update user data: $error");
-      throw Exception("Failed to update user data");
-    }
-  }
-
-  Future<void> updateUser(User user) async {
-    try {
-      SimpleLogger.info("Updating user");
-      await _usersCollection.doc(user.identidade).update(user.toMap());
-    } catch (error) {
-      SimpleLogger.warning("Failed to update user: $error");
-      throw Exception("Failed to update user");
-    }
-  }
-
-  Future<int> fetchNumero() async {
-    try {
-      QuerySnapshot querySnapshot = await _usersCollection
-          .orderBy('numero', descending: true)
-          .limit(1)
-          .get();
-      print(querySnapshot.docs[0].data());
-      if (querySnapshot.docs.isNotEmpty) {
-        var data = querySnapshot.docs[0].data() as Map<String, dynamic>;
-        return data['numero'] != null ? data['numero'] as int : 9999;
-      } else {
-        return 9999;
-      }
-    } catch (error) {
-      SimpleLogger.warning("Failed to fetch numero: $error");
-      throw Exception("Failed to fetch numero");
-    }
-  }
-
-  Future<bool> login(String identidade, String password) async {
-    try {
-      SimpleLogger.info("Logging in");
-      QuerySnapshot querySnapshot = await _usersCollection
-          .where('identidade', isEqualTo: identidade)
-          .get();
-      if (querySnapshot.docs.isNotEmpty) {
-        var data = querySnapshot.docs[0].data() as Map<String, dynamic>;
-        return data['password'] == password;
-      } else {
-        return false;
-      }
-    } catch (error) {
-      SimpleLogger.warning("Failed to login: $error");
-      throw Exception("Failed to login");
+    // Insert new data into the local database
+    for (var user in serverUsers) {
+      await localStorageService.insertData('users', user.toJson());
     }
   }
 }
