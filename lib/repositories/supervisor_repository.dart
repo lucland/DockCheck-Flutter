4c253443d9f489f1816466b4870e3bdece98fcd1
lucland <lucas.vsc@gmail.com>
2023-12-04 16:52:28 -0300

    Add SupervisorRepository class with CRUD operations and sync functionality

---
+++ lib/repositories/supervisor_repository.dart
@@ -0,0 +1,58 @@
+import 'package:cripto_qr_googlemarine/models/supervisor.dart';
+import 'package:cripto_qr_googlemarine/services/api_service.dart';
+import 'package:cripto_qr_googlemarine/services/local_storage_service.dart';
+
+class SupervisorRepository {
+  final ApiService apiService;
+  final LocalStorageService localStorageService;
+
+  SupervisorRepository(this.apiService, this.localStorageService);
+
+  Future<Supervisor> createSupervisor(Supervisor supervisor) async {
+    final data =
+        await apiService.post('supervisors/create', supervisor.toJson());
+    return Supervisor.fromJson(data);
+  }
+
+  Future<Supervisor> getSupervisor(String id) async {
+    final data = await apiService.get('supervisors/$id');
+    return Supervisor.fromJson(data);
+  }
+
+  Future<Supervisor> updateSupervisor(String id, Supervisor supervisor) async {
+    final data = await apiService.put('supervisors/$id', supervisor.toJson());
+    return Supervisor.fromJson(data);
+  }
+
+  Future<void> deleteSupervisor(String id) async {
+    await apiService.delete('supervisors/$id');
+  }
+
+  Future<List<Supervisor>> getAllSupervisors() async {
+    final data = await apiService.get('supervisors');
+    return (data as List).map((item) => Supervisor.fromJson(item)).toList();
+  }
+
+  // Sync supervisors from the server and update local database
+  Future<void> syncSupervisors() async {
+    try {
+      // Fetch data from the server
+      final serverSupervisors = await getAllSupervisors();
+
+      // Update local database
+      await updateLocalDatabase(serverSupervisors);
+    } catch (e) {
+      // Handle errors
+    }
+  }
+
+  Future<void> updateLocalDatabase(List<Supervisor> serverSupervisors) async {
+    // Clear local data
+    await localStorageService.clearTable('supervisors');
+
+    // Insert new data into the local database
+    for (var supervisor in serverSupervisors) {
+      await localStorageService.insertData('supervisors', supervisor.toJson());
+    }
+  }
+}
