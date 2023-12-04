ecf7c45b3ec33d3c04becf6d17a3d9488fe18cec
lucland <lucas.vsc@gmail.com>
2023-12-04 16:51:13 -0300

    Fix bug in login functionality

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -1,8 +1,3 @@
-import 'dart:typed_data';
-import 'dart:ui' as ui;
-
-import 'package:cripto_qr_googlemarine/blocs/home/home_cubit.dart';
-import 'package:cripto_qr_googlemarine/blocs/home/home_state.dart';
 import 'package:cripto_qr_googlemarine/utils/formatter.dart';
 import 'package:cripto_qr_googlemarine/utils/theme.dart';
 import 'package:cripto_qr_googlemarine/utils/ui/colors.dart';
@@ -10,24 +5,32 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:image_gallery_saver/image_gallery_saver.dart';
+import 'package:provider/provider.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 
 import '../../models/user.dart';
 
 import '../../repositories/user_repository.dart';
+import '../../services/local_storage_service.dart';
 import '../../utils/ui/strings.dart';
-import '../../widgets/print_button_widget.dart';
+import '../../widgets/sync_button_widget.dart';
 import '../../widgets/semi_circle_painter.dart';
 import '../../widgets/title_value_widget.dart';
+import 'cubit/home_cubit.dart';
+import 'cubit/home_state.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final UserRepository userRepository =
+        Provider.of<UserRepository>(context, listen: false);
+    final LocalStorageService localStorageService =
+        Provider.of<LocalStorageService>(context, listen: false);
+
     return BlocProvider(
-      create: (context) => HomeCubit(UserRepository()),
+      create: (context) => HomeCubit(userRepository, localStorageService),
       child: Container(
         color: CQColors.white,
         child: const HomeView(),
@@ -42,73 +45,13 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     context.read<HomeCubit>().fetchLastUser();
-    final GlobalKey globalKey = GlobalKey();
 
     return BlocBuilder<HomeCubit, HomeState>(
       builder: (context, state) {
-        Future<void> convertWidgetToImage() async {
-          try {
-            RenderRepaintBoundary boundary = globalKey.currentContext!
-                .findRenderObject() as RenderRepaintBoundary;
-
-            // Get the device's pixel ratio
-            final double devicePixelRatio =
-                MediaQuery.of(globalKey.currentContext!).devicePixelRatio;
-
-            // Convert mm to inches (1 inch = 25.4 mm)
-            //    final double heightInInches = 63 / 25.4;
-            const double widthInInches = 85 / 25.4;
-
-            // Calculate the target dimensions in pixels
-            //   final double targetHeightInPixels = heightInInches *
-            //     devicePixelRatio *
-            //    96; // 96 is the DPI for flutter in most cases
-            final double targetWidthInPixels =
-                widthInInches * devicePixelRatio * 96;
-
-            ui.Image image = await boundary.toImage(
-                pixelRatio: targetWidthInPixels / boundary.size.width);
-            ByteData? byteData =
-                await image.toByteData(format: ui.ImageByteFormat.png);
-            Uint8List pngBytes = byteData!.buffer.asUint8List();
-
-            //present image in dialog
-            showDialog(
-              context: globalKey.currentContext!,
-              builder: (context) => AlertDialog(
-                content: Image.memory(pngBytes),
-              ),
-            );
-
-            // final Image pngImage = Image.memory(pngBytes);
-
-            printPngImage(pngBytes);
-
-            final result = await ImageGallerySaver.saveImage(
-                pngBytes.buffer.asUint8List());
-            print(result);
-            //return snackbar
-            ScaffoldMessenger.of(globalKey.currentContext!).showSnackBar(
-              SnackBar(
-                content: Text(CQStrings.qrCodeSalvoNaGaleria,
-                    style: CQTheme.body.copyWith(color: CQColors.white)),
-                backgroundColor: CQColors.success100,
-              ),
-            );
-
-            // Now pngBytes contains the PNG image data with dimensions close to 63mm x 85mm.
-            // You can write it to a file or do something else with it.
-
-            //save to gallery
-          } catch (e) {
-            print(e);
-          }
-        }
-
         if (state.isLoading) {
           return const Center(child: CircularProgressIndicator());
         } else {
-          User user = state.user!;
+          User user = state.loggedUser!;
           return Container(
             color: CQColors.background,
             height: MediaQuery.of(context).size.height,
@@ -119,13 +62,14 @@ class HomeView extends StatelessWidget {
                 Padding(
                   padding: const EdgeInsets.all(16.0),
                   child: Text(
-                    CQStrings.benVindoABordo,
+                    'Olá, ${user.name}',
                     style: CQTheme.h3.copyWith(
                       color: Colors.black,
                       fontSize: 24,
                     ),
                   ),
                 ),
+
                 Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 16.0),
                   child: Container(
@@ -141,7 +85,6 @@ class HomeView extends StatelessWidget {
                             TicketHeader(user: user),
                             TicketBody(
                               user: user,
-                              data: user.toDatabaseString(),
                             ),
                           ],
                         ),
@@ -260,12 +203,31 @@ class HomeView extends StatelessWidget {
                   ),
                 ),*/
                 const Spacer(),
-                PrintButtonWidget(
-                  onPressed: () {
-                    //push to bluetooth page
-                    Navigator.pushNamed(context, '/bluetooth');
-                  },
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    SyncButtonWidget(
+                      onPressed: () {
+                        //push to bluetooth page
+                        Navigator.pushNamed(context, '/bluetooth');
+                      },
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                      child: Text(
+                        CQStrings.atualizadoEm(
+                            Formatter.formatDateTime(user.updatedAt)),
+                        style: CQTheme.body.copyWith(
+                          color: CQColors.slate100,
+                          fontSize: 14,
+                        ),
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                  ],
                 ),
+                const SizedBox(height: 16),
+                //text with 'last sync at
               ],
             ),
           );
@@ -279,11 +241,9 @@ class TicketBody extends StatelessWidget {
   const TicketBody({
     super.key,
     required this.user,
-    required this.data,
   });
 
   final User user;
-  final String data;
 
   @override
   Widget build(BuildContext context) {
@@ -300,36 +260,31 @@ class TicketBody extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                TitleValueWidget(
-                    title: CQStrings.embarcacao, value: user.vessel),
-                const SizedBox(height: 16),
-                TitleValueWidget(title: CQStrings.funcao, value: user.funcao),
-                const SizedBox(height: 16),
-                TitleValueWidget(title: CQStrings.empresa, value: user.empresa),
-              ],
-            ),
-          ),
-          const Divider(),
           Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Flexible(
                 flex: 1,
                 child: Padding(
                   padding: const EdgeInsets.all(8.0),
-                  child: EstadiaWidget(user: user),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      TitleValueWidget(
+                          title: CQStrings.empresa, value: user.company),
+                    ],
+                  ),
                 ),
               ),
               Flexible(
                 flex: 1,
-                child: QrCodeWidget(
-                  data: data,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(user.number.toString(),
+                      style: CQTheme.h1
+                          .copyWith(fontSize: 38, color: Colors.black)),
                 ),
               ),
             ],
@@ -366,7 +321,7 @@ class TicketHeader extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text(
-              user.nome,
+              'À bordo',
               style: CQTheme.h3.copyWith(
                 color: Colors.white,
               ),
@@ -405,8 +360,7 @@ class TicketFooter extends StatelessWidget {
             const Icon(Icons.info_outline, color: CQColors.slate100, size: 14),
             const SizedBox(width: 8),
             Text(
-              CQStrings.atualizadoEm(
-                  Formatter.formatDateTime(user.updatedAt.toDate())),
+              CQStrings.atualizadoEm(Formatter.formatDateTime(user.updatedAt)),
               style: CQTheme.body.copyWith(
                 color: CQColors.slate100,
                 fontSize: 14,
@@ -465,82 +419,11 @@ class EstadiaWidget extends StatelessWidget {
     return Column(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              CQStrings.estadia,
-              style: CQTheme.body.copyWith(
-                color: CQColors.slate100,
-                fontSize: 12,
-              ),
-            ),
-            const SizedBox(height: 4),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 4.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    CQStrings.de,
-                    style: CQTheme.body.copyWith(
-                      color: Colors.black,
-                      fontWeight: FontWeight.w600,
-                      fontSize: 14,
-                    ),
-                  ),
-                  Text(
-                    Formatter.formatDateTime(user.dataInicial.toDate()),
-                    style: CQTheme.body.copyWith(
-                      color: Colors.black,
-                      fontWeight: FontWeight.w700,
-                      fontSize: 14,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            const SizedBox(height: 4),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  CQStrings.ate,
-                  style: CQTheme.body.copyWith(
-                    color: Colors.black,
-                    fontWeight: FontWeight.w600,
-                    fontSize: 14,
-                  ),
-                ),
-                Text(
-                  Formatter.formatDateTime(user.dataLimite.toDate()),
-                  style: CQTheme.body.copyWith(
-                    color: Colors.black,
-                    fontWeight: FontWeight.w700,
-                    fontSize: 14,
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 16.0),
-          child: Text(user.numero.toString(),
+        Center(
+          child: Text(user.number.toString(),
               style: CQTheme.h1.copyWith(fontSize: 38, color: Colors.black)),
         ),
       ],
     );
   }
-}
-
-Future<void> printPngImage(Uint8List pngBytes) async {
-  //final profile = await CapabilityProfile.load();
-
-  // final printer = NetworkPrinter(PaperSize.mm80, profile);
-
-  //final connect = await printer.connect('192.168.0.123', port: 9100);
-
-  // printer.cut();
-  // printer.disconnect();
 }
