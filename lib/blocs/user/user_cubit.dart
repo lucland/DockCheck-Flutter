e5d1751e76513bcdb7d07fc9c20ce9b3f8f7cac7
lucland <lucas.vsc@gmail.com>
2023-12-04 16:49:08 -0300

    Refactor user_cubit.dart to use async/await syntax

--- lib/blocs/user/user_cubit.dart
+++ lib/blocs/user/user_cubit.dart
@@ -1,6 +1,5 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-import '../../models/user.dart';
 import '../../repositories/user_repository.dart';
 import 'user_state.dart';
 
@@ -9,30 +8,29 @@ class UserCubit extends Cubit<UserState> {
 
   UserCubit(this.userRepository) : super(UserInitial());
 
-  fetchUsers() async {
+  Future<void> fetchUsers() async {
     try {
       emit(UserLoading());
-      var users = await userRepository.fetchUsers();
-      print("Users fetched: ${users[0]}");
-      List<User> usersMapped =
-          users.map((userMap) => User.fromMap(userMap)).toList();
-      print(usersMapped[0].nome);
-      emit(UserLoaded(usersMapped));
+
+      // Fetch users with default limit and offset
+      var users = await userRepository.getAllUsers();
+
+      emit(UserLoaded(users));
     } catch (e) {
       print(e.toString());
       emit(UserError("Failed to fetch users."));
     }
   }
 
-  searchUsers(String query) async {
+  Future<void> searchUsers(String query) async {
     try {
       emit(UserLoading());
-      var users = await userRepository.searchUsers(query);
-      print("Users fetched: ${users[0]}");
-      List<User> usersMapped =
-          users.map((userMap) => User.fromMap(userMap)).toList();
-      print(usersMapped[0].nome);
-      emit(UserLoaded(usersMapped));
+      // Assuming the default page number and page size
+      int page = 1; // You can modify this as needed
+      int pageSize = 10; // You can modify this as needed
+
+      var users = await userRepository.searchUsers(query, page, pageSize);
+      emit(UserLoaded(users));
     } catch (e) {
       print(e.toString());
       emit(UserError("Failed to fetch users."));
