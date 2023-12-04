9f3947bdf1562227267244dff806dfb5f49f6b13
lucland <lucas.vsc@gmail.com>
2023-12-04 16:51:37 -0300

    Refactor login functionality and add error handling

--- lib/pages/login/cubit/login_cubit.dart
+++ lib/pages/login/cubit/login_cubit.dart
@@ -1,16 +1,52 @@
+import 'package:cripto_qr_googlemarine/repositories/user_repository.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
+import 'package:cripto_qr_googlemarine/repositories/login_repository.dart';
+import '../../../services/local_storage_service.dart';
 import 'login_state.dart';
 
 class LoginCubit extends Cubit<LoginState> {
-  LoginCubit() : super(LoginState.initial);
+  final LoginRepository loginRepository;
+  final UserRepository userRepository;
+  final LocalStorageService localStorageService;
 
-  Future<void> logIn(String email, String password) async {
-    emit(LoginState.loading);
+  LoginCubit(
+      this.loginRepository, this.userRepository, this.localStorageService)
+      : super(LoginInitial());
+
+  Future<void> logIn(String username, String password) async {
+    print(password + username);
+    print('login');
+    emit(LoginLoading());
     try {
-      emit(LoginState.success);
+      final response =
+          await loginRepository.login(username, password, 'admin', 'mobile');
+
+      await localStorageService.deleteUserId();
+
+      if (response.containsKey('token')) {
+        await localStorageService.saveToken(response['token']);
+        await localStorageService.saveUserId(response['user_id']);
+        final user = await userRepository.getUser(response['user_id']);
+        await localStorageService.saveUser(user);
+        emit(LoginSuccess(
+            userId: response['user_id'], token: response['token']));
+      } else {
+        print('token error');
+        emit(LoginError("Login failed: Token not found"));
+      }
     } catch (e) {
-      emit(LoginState.error);
+      print('error');
+      print(e.toString());
+      emit(LoginError("Login failed: ${e.toString()}"));
+    }
+  }
+
+  void validateToken(String token) {
+    if (token.isNotEmpty) {
+      // Assuming token is valid for demonstration
+      emit(LoginSuccess(userId: '', token: token));
+    } else {
+      emit(LoginError("Login failed: Token not found"));
     }
   }
 }
--- lib/pages/login/cubit/login_state.dart
+++ lib/pages/login/cubit/login_state.dart
@@ -1,1 +1,18 @@
-enum LoginState { initial, loading, success, error }
+abstract class LoginState {}
+
+class LoginInitial extends LoginState {}
+
+class LoginLoading extends LoginState {}
+
+class LoginSuccess extends LoginState {
+  final String userId;
+  final String token;
+
+  LoginSuccess({required this.userId, required this.token});
+}
+
+class LoginError extends LoginState {
+  final String message;
+
+  LoginError(this.message);
+}
