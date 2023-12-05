5881799d27ef58aca88c855dafd3c3b4757be876
lucland <lucas.vsc@gmail.com>
2023-12-05 12:43:17 -0300

    Add Vessel and OnboardUser lists to HomeState

--- lib/pages/home/cubit/home_state.dart
+++ lib/pages/home/cubit/home_state.dart
@@ -2,15 +2,22 @@ import 'dart:typed_data';
 
 import 'package:cripto_qr_googlemarine/models/user.dart';
 
+import '../../../models/vessel.dart';
+
 class HomeState {
   final Uint8List? pngBytes;
   final String? resultMessage;
   final User? loggedUser;
   final bool isLoading;
+  List<Vessel> vessels;
+  List<User> onboardUsers = [];
 
-  HomeState(
-      {this.pngBytes,
-      this.resultMessage,
-      this.loggedUser,
-      this.isLoading = true});
+  HomeState({
+    this.pngBytes,
+    this.resultMessage,
+    this.loggedUser,
+    this.isLoading = true,
+    this.vessels = const [],
+    this.onboardUsers = const [],
+  });
 }
