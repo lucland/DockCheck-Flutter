4b790c6e199cccf094bdb679cdd3073e39a4bb67
lucland <lucas.vsc@gmail.com>
2023-12-05 10:06:47 -0300

    Add data synchronization during login

--- lib/pages/init.dart
+++ lib/pages/init.dart
@@ -1,5 +1,13 @@
+import 'package:cripto_qr_googlemarine/utils/simple_logger.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import '../repositories/authorization_repository.dart';
+import '../repositories/company_repository.dart';
+import '../repositories/docking_repository.dart';
+import '../repositories/event_repository.dart';
+import '../repositories/portal_repository.dart';
+import '../repositories/user_repository.dart';
+import '../repositories/vessel_repository.dart';
 import '../services/local_storage_service.dart';
 import 'login/cubit/login_cubit.dart';
 import 'login/cubit/login_state.dart';
@@ -32,6 +40,22 @@ class _InitPageState extends State<InitPage> {
     }
   }
 
+  Future<void> _syncData() async {
+    // Call sync functions from all repositories
+    try {
+      context.read<AuthorizationRepository>().syncAuthorizations();
+      context.read<CompanyRepository>().syncCompanies();
+      context.read<DockingRepository>().syncDockings();
+      context.read<EventRepository>().syncEvents();
+      context.read<PortalRepository>().syncPortals();
+      context.read<UserRepository>().syncUsers();
+      context.read<VesselRepository>().syncVessels();
+    } catch (e) {
+      SimpleLogger.warning('Error during data synchronization: $e');
+      print('Error during data synchronization: $e');
+    }
+  }
+
   void _navigateToLoginPage() {
     Navigator.of(context).pushReplacement(
       MaterialPageRoute(builder: (_) => LoginPage()),
@@ -49,7 +73,10 @@ class _InitPageState extends State<InitPage> {
     return BlocListener<LoginCubit, LoginState>(
       listener: (context, state) {
         if (state is LoginSuccess) {
-          _navigateToRootPage();
+          _syncData().then((_) {
+            SimpleLogger.info('Data synchronization completed');
+            _navigateToRootPage();
+          });
         } else if (state is LoginError) {
           _navigateToLoginPage();
         }
