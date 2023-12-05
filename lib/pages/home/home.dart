90055d49850e094af44a59973150c8de3c653736
lucland <lucas.vsc@gmail.com>
2023-12-05 12:43:06 -0300

    New vessel list with onboarded info

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -1,15 +1,15 @@
+import 'package:cripto_qr_googlemarine/repositories/vessel_repository.dart';
 import 'package:cripto_qr_googlemarine/utils/formatter.dart';
 import 'package:cripto_qr_googlemarine/utils/theme.dart';
 import 'package:cripto_qr_googlemarine/utils/ui/colors.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:provider/provider.dart';
-import 'package:qr_flutter/qr_flutter.dart';
 
 import '../../models/user.dart';
 
+import '../../models/vessel.dart';
+import '../../repositories/authorization_repository.dart';
 import '../../repositories/user_repository.dart';
 import '../../services/local_storage_service.dart';
 import '../../utils/ui/strings.dart';
@@ -28,9 +28,14 @@ class Home extends StatelessWidget {
         Provider.of<UserRepository>(context, listen: false);
     final LocalStorageService localStorageService =
         Provider.of<LocalStorageService>(context, listen: false);
+    final VesselRepository vesselRepository =
+        Provider.of<VesselRepository>(context, listen: false);
+    final AuthorizationRepository authorizationRepository =
+        Provider.of<AuthorizationRepository>(context, listen: false);
 
     return BlocProvider(
-      create: (context) => HomeCubit(userRepository, localStorageService),
+      create: (context) => HomeCubit(userRepository, localStorageService,
+          vesselRepository, authorizationRepository),
       child: Container(
         color: CQColors.white,
         child: const HomeView(),
@@ -44,13 +49,14 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    context.read<HomeCubit>().fetchLastUser();
+    context.read<HomeCubit>().fetchLoggedUser();
 
     return BlocBuilder<HomeCubit, HomeState>(
       builder: (context, state) {
         if (state.isLoading) {
           return const Center(child: CircularProgressIndicator());
-        } else {
+        } else if (state.isLoading == false && state.loggedUser != null) {
+          List<User> users = state.onboardUsers;
           User user = state.loggedUser!;
           return Container(
             color: CQColors.background,
@@ -69,170 +75,146 @@ class HomeView extends StatelessWidget {
                     ),
                   ),
                 ),
-
                 Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                  child: Container(
-                    decoration: const BoxDecoration(
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(10.0),
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 16.0, vertical: 2.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Pessoas a bordo:',
+                        style: CQTheme.h3.copyWith(
+                          color: Colors.black,
+                          fontSize: 16,
+                        ),
                       ),
-                    ),
-                    child: Stack(
-                      children: [
-                        Column(
-                          children: [
-                            TicketHeader(user: user),
-                            TicketBody(
-                              user: user,
-                            ),
-                          ],
+                      Text(
+                        'Total: ${users.length}',
+                        style: CQTheme.h3.copyWith(
+                          color: Colors.black,
+                          fontSize: 24,
                         ),
-                        Positioned(
-                          top: 48,
-                          left: -10,
-                          child: CustomPaint(
-                            size: const Size(20, 10),
-                            painter: CirclePainter(
-                              color: CQColors.background,
-                              direction: Direction.left,
-                            ),
-                          ),
-                        ),
-                        Positioned(
-                          top: 48,
-                          right: -10,
-                          child: CustomPaint(
-                            size: const Size(20, 10),
-                            painter: CirclePainter(
-                              color: CQColors.background,
-                              direction: Direction.right,
-                            ),
-                          ),
-                        ),
-                        Positioned(
-                          bottom: 48,
-                          left: -10,
-                          child: CustomPaint(
-                            size: const Size(20, 10),
-                            painter: CirclePainter(
-                              color: CQColors.background,
-                              direction: Direction.right,
-                            ),
-                          ),
-                        ),
-                        Positioned(
-                          bottom: 48,
-                          right: -10,
-                          child: CustomPaint(
-                            size: const Size(20, 10),
-                            painter: CirclePainter(
-                              color: CQColors.background,
-                              direction: Direction.left,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
                 ),
-                /*  RepaintBoundary(
-                  key: globalKey,
-                  child: Card(
-                    color: Colors.white,
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Flexible(
-                          flex: 1,
-                          child: Padding(
-                            padding:
-                                const EdgeInsets.fromLTRB(16.0, 16, 0, 16),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                Text(user.nome,
-                                    style: CQTheme.h1.copyWith(
-                                      fontSize: 10,
-                                    )),
-                                Text(
-                                  user.funcao,
-                                  style: CQTheme.h2.copyWith(
-                                    fontSize: 10,
-                                  ),
-                                ),
-                                Text(
-                                  user.empresa,
-                                  style: CQTheme.h3.copyWith(
-                                    fontSize: 10,
-                                  ),
-                                ),
-                                Text(
-                                  user.numero.toString(),
-                                  style: CQTheme.h1.copyWith(
-                                    fontSize: 16,
-                                  ),
-                                ),
-                                Text(
-                                  user.identidade,
-                                  style: CQTheme.subhead1.copyWith(
-                                    fontSize: 10,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                        Flexible(
-                          flex: 2,
-                          child: Padding(
-                            padding: const EdgeInsets.fromLTRB(0, 8, 8, 8),
-                            child: QrImageView(
-                              data: state.user!.toDatabaseString(),
-                              version: QrVersions.auto,
-                              size: 300.0,
-                            ),
-                          ),
-                        ),
-                      ],
+                const Divider(),
+                //list of onboard tickets for each vessel
+                if (state.vessels.isNotEmpty)
+                  Expanded(
+                    child: ListView.builder(
+                      physics: const BouncingScrollPhysics(),
+                      shrinkWrap: true,
+                      itemCount: state.vessels.length,
+                      itemBuilder: (context, index) {
+                        return Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: OnboardTicket(
+                              users: users, vessel: state.vessels[index]),
+                        );
+                      },
                     ),
                   ),
-                ),*/
-                const Spacer(),
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
                     SyncButtonWidget(
                       onPressed: () {
-                        //push to bluetooth page
-                        Navigator.pushNamed(context, '/bluetooth');
+                        context.read<HomeCubit>().reloadPage();
                       },
                     ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                      child: Text(
-                        CQStrings.atualizadoEm(
-                            Formatter.formatDateTime(user.updatedAt)),
-                        style: CQTheme.body.copyWith(
-                          color: CQColors.slate100,
-                          fontSize: 14,
-                        ),
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                    ),
+                    const SizedBox(height: 4),
                   ],
                 ),
-                const SizedBox(height: 16),
-                //text with 'last sync at
               ],
             ),
           );
+        } else {
+          return const Center(child: CircularProgressIndicator());
         }
       },
+    );
+  }
+}
+
+class OnboardTicket extends StatelessWidget {
+  const OnboardTicket({
+    super.key,
+    required this.users,
+    required this.vessel,
+  });
+
+  final List<User> users;
+  final Vessel vessel;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 0.0),
+      child: Container(
+        decoration: const BoxDecoration(
+          borderRadius: BorderRadius.all(
+            Radius.circular(10.0),
+          ),
+        ),
+        child: Stack(
+          children: [
+            Column(
+              children: [
+                TicketHeader(vessel: vessel),
+                TicketBody(
+                  users: users,
+                  vessel: vessel,
+                ),
+              ],
+            ),
+            Positioned(
+              top: 48,
+              left: -10,
+              child: CustomPaint(
+                size: const Size(20, 10),
+                painter: CirclePainter(
+                  color: CQColors.background,
+                  direction: Direction.left,
+                ),
+              ),
+            ),
+            Positioned(
+              top: 48,
+              right: -10,
+              child: CustomPaint(
+                size: const Size(20, 10),
+                painter: CirclePainter(
+                  color: CQColors.background,
+                  direction: Direction.right,
+                ),
+              ),
+            ),
+            Positioned(
+              bottom: 48,
+              left: -10,
+              child: CustomPaint(
+                size: const Size(20, 10),
+                painter: CirclePainter(
+                  color: CQColors.background,
+                  direction: Direction.right,
+                ),
+              ),
+            ),
+            Positioned(
+              bottom: 48,
+              right: -10,
+              child: CustomPaint(
+                size: const Size(20, 10),
+                painter: CirclePainter(
+                  color: CQColors.background,
+                  direction: Direction.left,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
@@ -240,10 +222,12 @@ class HomeView extends StatelessWidget {
 class TicketBody extends StatelessWidget {
   const TicketBody({
     super.key,
-    required this.user,
+    required this.users,
+    required this.vessel,
   });
 
-  final User user;
+  final List<User> users;
+  final Vessel vessel;
 
   @override
   Widget build(BuildContext context) {
@@ -260,37 +244,53 @@ class TicketBody extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Flexible(
-                flex: 1,
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      TitleValueWidget(
-                          title: CQStrings.empresa, value: user.company),
-                    ],
-                  ),
+          //text saying Total: ${users.length}
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: Text(
+                'Total: ${users.length}',
+                style: CQTheme.h3.copyWith(
+                  color: Colors.black,
+                  fontSize: 16,
                 ),
               ),
-              Flexible(
-                flex: 1,
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(user.number.toString(),
-                      style: CQTheme.h1
-                          .copyWith(fontSize: 38, color: Colors.black)),
-                ),
-              ),
-            ],
+            ),
           ),
           const Divider(),
-          TicketFooter(user: user),
+          ListView.builder(
+            shrinkWrap: true,
+            itemCount: users.length,
+            physics: const NeverScrollableScrollPhysics(),
+            itemBuilder: (context, index) {
+              return Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            TitleValueWidget(
+                                title: "Nome", value: users[index].name),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(users[index].area.toString(),
+                          style: CQTheme.h1.copyWith(
+                              fontSize: 14, color: CQColors.success100)),
+                    ),
+                  ]);
+            },
+          ),
+          const Divider(),
+          TicketFooter(updatedAt: vessel.updatedAt),
         ],
       ),
     );
@@ -300,10 +300,10 @@ class TicketBody extends StatelessWidget {
 class TicketHeader extends StatelessWidget {
   const TicketHeader({
     super.key,
-    required this.user,
+    required this.vessel,
   });
 
-  final User user;
+  final Vessel vessel;
 
   @override
   Widget build(BuildContext context) {
@@ -321,7 +321,7 @@ class TicketHeader extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text(
-              'À bordo',
+              vessel.name,
               style: CQTheme.h3.copyWith(
                 color: Colors.white,
               ),
@@ -337,10 +337,10 @@ class TicketHeader extends StatelessWidget {
 class TicketFooter extends StatelessWidget {
   const TicketFooter({
     super.key,
-    required this.user,
+    required this.updatedAt,
   });
 
-  final User user;
+  final DateTime updatedAt;
 
   @override
   Widget build(BuildContext context) {
@@ -360,7 +360,7 @@ class TicketFooter extends StatelessWidget {
             const Icon(Icons.info_outline, color: CQColors.slate100, size: 14),
             const SizedBox(width: 8),
             Text(
-              CQStrings.atualizadoEm(Formatter.formatDateTime(user.updatedAt)),
+              CQStrings.atualizadoEm(Formatter.formatDateTime(updatedAt)),
               style: CQTheme.body.copyWith(
                 color: CQColors.slate100,
                 fontSize: 14,
@@ -370,38 +370,6 @@ class TicketFooter extends StatelessWidget {
           ],
         ),
       ),
-    );
-  }
-}
-
-class QrCodeWidget extends StatelessWidget {
-  final String data;
-  const QrCodeWidget({
-    super.key,
-    required this.data,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Expanded(
-          child: QrImageView(
-            data: data,
-            version: QrVersions.auto,
-          ),
-        ),
-        RotatedBox(
-          quarterTurns: 1,
-          child: Text(
-            CQStrings.poweredBy,
-            style: CQTheme.body.copyWith(
-              color: CQColors.slate100,
-              fontSize: 10,
-            ),
-          ),
-        ),
-      ],
     );
   }
 }
