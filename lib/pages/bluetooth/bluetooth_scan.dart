a3610cecb8e915317a4883e9e96bd8c3da8634af
lucland <lucas.vsc@gmail.com>
2023-12-08 17:55:47 -0300

    Add Bluetooth scan screen

---
+++ lib/pages/bluetooth/bluetooth_scan.dart
@@ -0,0 +1,154 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:cripto_qr_googlemarine/pages/bluetooth/widgets/streams.dart';
+import 'package:cripto_qr_googlemarine/utils/ui/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_blue_plus/flutter_blue_plus.dart';
+import 'device_screen.dart'; // Make sure to import the DeviceScreen
+import '../../utils/simple_logger.dart';
+import 'widgets/scan_result_title.dart';
+import 'widgets/system_device_title.dart'; // Import your utilities
+
+class ScanScreen extends StatefulWidget {
+  const ScanScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ScanScreen> createState() => _ScanScreenState();
+}
+
+class _ScanScreenState extends State<ScanScreen> {
+  List<BluetoothDevice> _systemDevices = [];
+  List<ScanResult> _scanResults = [];
+  bool _isScanning = false;
+  late StreamSubscription<List<ScanResult>> _scanResultsSubscription;
+  late StreamSubscription<bool> _isScanningSubscription;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _scanResultsSubscription = FlutterBluePlus.scanResults.listen((results) {
+      setState(() {
+        _scanResults = results;
+      });
+    });
+
+    _isScanningSubscription = FlutterBluePlus.isScanning.listen((state) {
+      setState(() {
+        _isScanning = state;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _scanResultsSubscription.cancel();
+    _isScanningSubscription.cancel();
+    super.dispose();
+  }
+
+  Future onScanPressed() async {
+    try {
+      _systemDevices = await FlutterBluePlus.systemDevices;
+    } catch (e) {
+      SimpleLogger.warning('Error during systemDevices: $e');
+    }
+
+    try {
+      // android is slow when asking for all advertisments,
+      // so instead we only ask for 1/8 of them
+      int divisor = Platform.isAndroid ? 8 : 1;
+      await FlutterBluePlus.startScan(
+          timeout: const Duration(seconds: 15),
+          continuousUpdates: true,
+          continuousDivisor: divisor);
+    } catch (e) {
+      SimpleLogger.warning('Error during startScan: $e');
+    }
+    setState(() {}); // force refresh of systemDevices
+  }
+
+  Future onStopPressed() async {
+    try {
+      FlutterBluePlus.stopScan();
+    } catch (e) {
+      SimpleLogger.warning('Error during stopScan: $e');
+    }
+  }
+
+  void onConnectPressed(BluetoothDevice device) {
+    device.connectAndUpdateStream().catchError((e) {});
+    MaterialPageRoute route = MaterialPageRoute(
+        builder: (context) => DeviceScreen(device: device),
+        settings: const RouteSettings(name: '/DeviceScreen'));
+    Navigator.of(context).push(route);
+  }
+
+  Future onRefresh() {
+    if (_isScanning == false) {
+      FlutterBluePlus.startScan(timeout: const Duration(seconds: 15));
+    }
+    setState(() {});
+    return Future.delayed(const Duration(milliseconds: 500));
+  }
+
+  Widget buildScanButton(BuildContext context) {
+    if (FlutterBluePlus.isScanningNow) {
+      return FloatingActionButton(
+        onPressed: onStopPressed,
+        backgroundColor: CQColors.danger100,
+        child: const Icon(Icons.stop),
+      );
+    } else {
+      return FloatingActionButton(
+          backgroundColor: CQColors.iron100,
+          onPressed: onScanPressed,
+          child: Icon(Icons.sensors_rounded, size: 30, color: CQColors.white));
+    }
+  }
+
+  List<Widget> _buildSystemDeviceTiles(BuildContext context) {
+    return _systemDevices
+        .map(
+          (d) => SystemDeviceTile(
+            device: d,
+            onOpen: () => Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) => DeviceScreen(device: d),
+                settings: const RouteSettings(name: '/DeviceScreen'),
+              ),
+            ),
+            onConnect: () => onConnectPressed(d),
+          ),
+        )
+        .toList();
+  }
+
+  List<Widget> _buildScanResultTiles(BuildContext context) {
+    return _scanResults
+        .map(
+          (r) => ScanResultTile(
+            result: r,
+            onTap: () => onConnectPressed(r.device),
+          ),
+        )
+        .toList();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: RefreshIndicator(
+        onRefresh: onRefresh,
+        child: ListView(
+          children: <Widget>[
+            ..._buildSystemDeviceTiles(context),
+            ..._buildScanResultTiles(context),
+          ],
+        ),
+      ),
+      floatingActionButton: buildScanButton(context),
+    );
+  }
+}
