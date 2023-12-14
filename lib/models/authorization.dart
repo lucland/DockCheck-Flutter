6a412aeb6a1fa7a6d8aa28d77da44cda7d0de484
lucland <lucas.vsc@gmail.com>
2023-12-14 18:13:43 -0300

    Add status field to Authorization model

--- lib/models/authorization.dart
+++ lib/models/authorization.dart
@@ -3,12 +3,14 @@ class Authorization {
   String userId;
   String vesselId;
   DateTime expirationDate;
+  String status;
 
   Authorization(
       {required this.id,
       required this.userId,
       required this.vesselId,
-      required this.expirationDate});
+      required this.expirationDate,
+      required this.status});
 
   factory Authorization.fromJson(Map<String, dynamic> json) {
     return Authorization(
@@ -16,6 +18,7 @@ class Authorization {
       userId: json['user_id'],
       vesselId: json['vessel_id'],
       expirationDate: DateTime.parse(json['expiration_date']),
+      status: json['status'],
     );
   }
 
@@ -25,6 +28,7 @@ class Authorization {
       'user_id': userId,
       'vessel_id': vesselId,
       'expiration_date': expirationDate.toIso8601String(),
+      'status': status,
     };
   }
 }
