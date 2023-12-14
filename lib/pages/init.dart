657c961e2d4ee36884b809f1eefac2118f2fb5f8
lucland <lucas.vsc@gmail.com>
2023-12-14 18:23:34 -0300

    Update import statement in init.dart

--- lib/pages/init.dart
+++ lib/pages/init.dart
@@ -1,4 +1,4 @@
-import 'package:cripto_qr_googlemarine/utils/simple_logger.dart';
+import 'package:dockcheck/utils/simple_logger.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import '../repositories/authorization_repository.dart';
