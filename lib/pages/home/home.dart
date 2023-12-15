eb323520564a9165a16c38da192c9d59a64f3b1f
lucland <lucas.vsc@gmail.com>
2023-12-15 17:05:10 -0300

    Update import statements in home.dart

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -1,7 +1,7 @@
-import 'package:cripto_qr_googlemarine/repositories/vessel_repository.dart';
-import 'package:cripto_qr_googlemarine/utils/formatter.dart';
-import 'package:cripto_qr_googlemarine/utils/theme.dart';
-import 'package:cripto_qr_googlemarine/utils/ui/colors.dart';
+import 'package:dockcheck/repositories/vessel_repository.dart';
+import 'package:dockcheck/utils/formatter.dart';
+import 'package:dockcheck/utils/theme.dart';
+import 'package:dockcheck/utils/ui/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:provider/provider.dart';
