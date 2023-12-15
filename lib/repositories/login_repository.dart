eb0ac828153d4fb0693a155f43d42e198b250495
lucland <lucas.vsc@gmail.com>
2023-12-15 17:07:56 -0300

    Refactor login and logout methods in LoginRepository

--- lib/repositories/login_repository.dart
+++ lib/repositories/login_repository.dart
@@ -1,8 +1,7 @@
 import 'dart:convert';
 
-import 'package:cripto_qr_googlemarine/utils/simple_logger.dart';
-
 import '../services/api_service.dart';
+import '../utils/simple_logger.dart';
 
 class LoginRepository {
   final ApiService apiService;
@@ -11,47 +10,68 @@ class LoginRepository {
 
   Future<Map<String, dynamic>> login(
       String username, String password, String role, String system) async {
-    final response = await apiService.postLogin(
-      'login',
-      {
-        'username': username,
-        'password': password,
-        'role': role,
-        'system': system,
-      },
-    );
-    if (response.statusCode == 200) {
-      SimpleLogger.info("User logged in");
-    } else {
-      SimpleLogger.warning("User login failed");
-    }
+    try {
+      final response = await apiService.postLogin(
+        'login',
+        {
+          'username': username,
+          'password': password,
+          'role': role,
+          'system': system,
+        },
+      );
 
-    return jsonDecode(response.body);
+      if (response.containsKey('error')) {
+        SimpleLogger.warning("User login failed");
+        return response; // Contains error details
+      } else {
+        SimpleLogger.info("User logged in");
+        await _storeUserData(response); // Store user data on successful login
+        return response; // Successful login data
+      }
+    } catch (e) {
+      SimpleLogger.severe("Login failed: ${e.toString()}");
+      return {'error': 'Login failed', 'details': e.toString()};
+    }
   }
 
   Future<bool> logout() async {
     final userId = await apiService.localStorageService.getUserId();
-
     if (userId == null || userId.isEmpty) {
-      print("User ID is null or empty");
       SimpleLogger.warning("User ID is null or empty");
       return false;
     }
 
-    final response = await apiService.postLogin(
-      'login/logout',
-      {'user_id': userId},
-    );
+    try {
+      final response = await apiService.postLogin(
+        'logout', // Assuming the endpoint for logout is 'logout'
+        {'user_id': userId},
+      );
 
-    if (response.statusCode == 200) {
-      await apiService.localStorageService.deleteToken();
-      await apiService.localStorageService.deleteUser();
-      await apiService.localStorageService.deleteUsername();
-      await apiService.localStorageService.deleteUserId();
-      SimpleLogger.info("User logged out");
-      return true;
+      if (response.containsKey('error')) {
+        SimpleLogger.warning("User logout failed");
+        return false;
+      } else {
+        SimpleLogger.info("User logged out");
+        await _clearUserData();
+        return true;
+      }
+    } catch (e) {
+      SimpleLogger.severe("Logout failed: ${e.toString()}");
+      return false;
     }
+  }
 
-    return false;
+  Future<void> _storeUserData(Map<String, dynamic> userData) async {
+    await apiService.localStorageService.saveToken(userData['token']);
+    await apiService.localStorageService
+        .saveUserId(userData['user_id'].toString());
+    // Store other user data as needed
+  }
+
+  Future<void> _clearUserData() async {
+    await apiService.localStorageService.deleteToken();
+    await apiService.localStorageService.deleteUserId();
+    // Clear other user data as needed
   }
 }
